1b999728168a214f8348b6ef7df52eb1461ed00b
shirinnizan <shirinnizan@gmail.com>
2022-09-26 12:04:19 -0400

    fix calculate function and added new css

--- app.js
+++ app.js
@@ -1,59 +1,81 @@
-const keys = document.querySelector(".calc-keys")
+//calculate function
+const calculate = (firstNumber, operator, secondNumber) => {
+    let result = "";
+
+    if (operator === "add") {
+        result = parseFloat(firstNumber) + parseFloat(secondNumber);
+    }
+    else if (operator === "subtract") {
+        result = parseFloat(firstNumber) - parseFloat(secondNumber);
+    }
+    else if (operator === "multiply") {
+        result = parseFloat(firstNumber) * parseFloat(secondNumber);
+    }
+    else if (operator === "divide") {
+        result = parseFloat(firstNumber) / parseFloat(secondNumber);
+    }
+    else if (operator === "percentage") {
+        result = parseFloat(firstNumber) * parseFloat(secondNumber) / 100;
+    }
+
+    return result;
+};
+
+const squareroot = (number, operator) => {
+    return Math.sqrt(number);
+}
+
 const calculator = document.querySelector(".calculator");
+const display = calculator.querySelector(".calc-display");
+const keys = calculator.querySelector(".calc-keys");
 
-
-//all the action keys in click event
-keys.addEventListener("click", (event) =>{
-
-    if(event.target.matches("button")){
-        const key = event.target;
+keys.addEventListener("click", e => {
+    if (e.target.matches("button")) {
+        const key = e.target;
         const action = key.dataset.action;
-       if (!action) {
-            console.log("number")
-          }
-
-          if (
-                action === 'add' ||
-                action === 'subtract' ||
-                action === 'multiply' ||
-                action === 'divide' ||
-                action === 'percentage' ||
-                action === 'squareroot'
-              ) {
-                console.log("operator")
-              }
-
-              if (action === 'decimal') {
-                console.log("decimal")
-              }
-              
-              if (action === 'clear') {
-                console.log("clear")
-              }
-              
-              if (action === 'calculate') {
-                console.log("equal")
-              }
-    }
-});
-
-const display = document.querySelector(".calc-display")
-
-keys.addEventListener("click", (event) =>{
-
-    if(event.target.matches("button")){
-        const key = event.target;
-        const action = key.dataset.action;
-        const keyContent = key.dataset.action;
+        const keyContent = key.textContent;
         const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
 
-          if (!action) {
-            if (displayedNum === "0") {
-              display.textContent = keyContent
+        if (!action) {
+            console.log("number keys");
+            if (displayedNum === "0" || previousKeyType === "operator") {
+                display.textContent = keyContent;
             } else {
-              display.textContent = displayedNum + keyContent
+                display.textContent = displayedNum + keyContent;
             }
-          }
+        }
+
+        if (action === "decimal") {
+            display.textContent = displayedNum + ".";
+        }
+
+        if ( action === "add" || action === "subtract" || action === "multiply" ||
+            action === "divide" || action === "percentage" || action === "squareroot") 
+        {
+            calculator.dataset.previousKeyType = "operator";
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
+        }
+
+        if (action === "clear") {
+            // display.innerHTML = "";
+            display.textContent = "0";
+            console.log("clear key")
+        }
+        if (action === "squareroot") {
+            const number = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            display.textContent = squareroot(number, operator);
+        }
+
+        if (action === "calculate") {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+
+            display.textContent = calculate(firstValue, operator, secondValue);
+        }
     }
 });
 
@@ -61,4 +83,4 @@ keys.addEventListener("click", (event) =>{
 
 
 
- 
+
